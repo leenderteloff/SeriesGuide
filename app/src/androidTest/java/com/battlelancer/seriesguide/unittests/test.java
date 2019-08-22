667e3155a853faeca40c6b2fe384b3caa7deefbf
leenderteloff <leenderteloff@gmail.com>
2019-08-22 15:13:43 +0200

    testing

--- app/src/androidTest/java/com/battlelancer/seriesguide/unittests/test.java
+++ app/src/androidTest/java/com/battlelancer/seriesguide/unittests/test.java
@@ -1,6 +1,7 @@
 package com.battlelancer.seriesguide.unittests;
 
 import com.battlelancer.seriesguide.ui.movies.MovieDetails;
+import com.battlelancer.seriesguide.ui.search.SearchResult;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,21 +21,59 @@ import static com.google.common.truth.Truth.*;
 @RunWith(MockitoJUnitRunner.class)
 public class test {
 
-    @Spy
+
     MovieDetails movieDetails;
+    SearchResult searchResult;
+
+
+    @Before
+    public void setup(){
+        searchResult = new SearchResult();
+        searchResult.setTitle("Avengers");
+        searchResult.setLanguage("English");
+    }
+
 
 
     @Test
-    public void setMovieToWatched(){
-        MockitoAnnotations.initMocks(this);
-     //   String movie = movieDetails.tmdbMovie().title = "Avengers";
+    public void  setSearchResult(){
+        Assert.assertEquals(searchResult.getTitle(), "Avengers");
+        Assert.assertEquals(searchResult.getLanguage(), "English");
+    }
+
+
+    @Test
+    public void setMovieRating(){
         movieDetails = new MovieDetails();
         movieDetails.setUserRating(1);
 
-      //  assertThat(movieDetails.getUserRating());
         Assert.assertEquals(movieDetails.getUserRating(), 1);
-
     }
 
+    @Test
+    public void setMovieWatched(){
+        movieDetails = new MovieDetails();
+        movieDetails.setWatched(true);
+        movieDetails.isWatched();
+
+        Assert.assertTrue(movieDetails.isWatched());
+    }
+
+    @Test
+    public void setMovieInCollection(){
+        movieDetails = new MovieDetails();
+        movieDetails.setInCollection(true);
+        movieDetails.isInCollection();
+
+        Assert.assertTrue(movieDetails.isInCollection());
+    }
+
+    @Test
+    public void setMovieInWatchlist(){
+        movieDetails = new MovieDetails();
+        movieDetails.setInWatchlist(true);
+        movieDetails.isInWatchlist();
+        Assert.assertTrue(movieDetails.isInWatchlist());
+    }
 
 }
